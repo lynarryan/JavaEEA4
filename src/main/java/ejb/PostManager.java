26abc9f073f3888cddda87fd01370ea76f01e961
Ryan Lynar <lynarryan@gmail.com>
2019-04-11 10:31:48 -0400

    Added persistence

--- src/main/java/ejb/PostManager.java
+++ src/main/java/ejb/PostManager.java
@@ -1,8 +1,13 @@
 package ejb;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Stateless
 public class PostManager extends ManagerBeans {
+    
+    @PersistenceContext(name= "assignment4")
+    protected EntityManager em;
 
 }
