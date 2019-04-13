11ad2c63a438167d2ec97bf3d301c9c8d2173f71
Greg <leve0292@algonquinlive.com>
2019-04-13 11:07:40 -0400

    Added test suites for Blog and BlogUser

--- src/main/java/models/Blog_.java
+++ src/main/java/models/Blog_.java
@@ -5,9 +5,10 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2019-04-10T18:18:48.566+0000")
+@Generated(value="Dali", date="2019-04-12T13:53:30.700+0000")
 @StaticMetamodel(Blog.class)
 public class Blog_ extends ModelBase_ {
 	public static volatile ListAttribute<Blog, BlogPost> blogs;
 	public static volatile SingularAttribute<Blog, BlogUser> blogUser;
+	public static volatile SingularAttribute<Blog, String> blogName;
 }
